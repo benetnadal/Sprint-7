e0dfc8a4adbcb0ecbed9f128dcc07e7ebf8e774e
benetnadal <benetnadal@gmail.com>
2023-06-18 13:15:43 +0200

    Exercici 2 - Sprint7

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import './App.css';
+//import './App.css';
 
 import {useEffect, useState} from "react";
+
+import {Panell} from "./styled"
 
 
 function App() {
@@ -9,15 +11,21 @@ const [web, setWeb] = useState(false);
 const [seo, setSeo] = useState(false);
 const [ads, setAds] = useState(false);
 
-const handleWeb = () => {setWeb(!web)}; // CANVI D´ESTAT A TRUE QUAN FEM CHECKBOX
+const handleWeb = () => {return (setWeb(!web), setPage(0))}; 
 const handleSeo = () => {setSeo(!seo)};
 const handleAds = () => {setAds(!ads)};
 
 const [budget, setBudget] = useState(0);
 
-const suma = () =>  {return ((!web? 0:500) + (!seo? 0:300) + (!ads? 0:200))};
+const [page, setPage] = useState(0) 
+const [idiom, setIdiom] = useState(0) 
+const [fix, setFix] = useState(0) 
 
-useEffect(() => {setBudget(suma()); console.log(budget)}) //GUARDEM PRESSUPOST A BUDGET
+const suma = () =>  {return ((!web? 0:500) + (!seo? 0:300) + (!ads? 0:200)) };
+const sumaWeb = () => {return ((!web? 0:page) * (!web? 0:idiom) * (fix))}
+
+
+useEffect(() => {setBudget((suma()) + (sumaWeb())); console.log(budget)}) //GUARDEM PRESSUPOST A BUDGET
 
 return (
 
@@ -28,10 +36,21 @@ return (
 
 <form>
   <label>
-        <input type="checkbox" checked={web} onChange= {handleWeb} />
+        <input type="checkbox"  checked={web}  onChange= {handleWeb} />
         Una pàgina Web (500€)
 
-  </label>
+    {web &&   <Panell> 
+      <p>Siusplau Omple les 2 caselles</p>
+          <p>Número de pàgines <input type="text"  onChange={(event) => {
+            return(setPage(event.target.value),setFix(30))}}/></p>
+
+        <p>Número d´idiomes <input type="text"   onChange={(event) => {
+        return(setIdiom(event.target.value),setFix(30))}}/></p>
+
+    </Panell> }  
+
+ </label>
+   
 </form>
 
 <form>
@@ -49,7 +68,7 @@ return (
 </form>
 
 <p>
-Preu: {suma()} 
+Preu: {suma() + sumaWeb()} 
 </p>
         
   </div>
---
+++ src/styled.js
@@ -0,0 +1,22 @@
+
+import styled from "styled-components"
+
+export const Panell = styled.div`
+width: 30%;
+margin: 35px;
+border: 4px solid black;
+border-radius: 14px;
+padding: 20px;
+`
+
+/* export const Input = styled.input`
+
+border: 2px solid green;
+color:red
+
+`
+export const InputMaxLengthNumber = styled("input").attrs({ maxLength: 2 })`
+border: 2px solid green;
+color:red
+`;
+ */
